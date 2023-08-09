880db92798331d6effcc57f6ad30974f8b5ff9c8
stas-bukovskiy <stbukovskiy12@gmail.com>
2023-08-09 13:05:08 +0300

    feat: initialize react project

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import LoginPage from "./components/Login/LoginPage.tsx";
+
+function App() {
+    return (
+        <>
+            <LoginPage></LoginPage>
+        </>
+    )
+}
+
+export default App
